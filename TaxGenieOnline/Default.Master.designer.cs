26d4b68190d447308ad9218ea8e3e82212d17ecf
ukotaru <mymailid.uma@gmail.com>
2012-09-17 11:47:28 +0530

    Case law search logo added

--- TaxGenieOnline/Default.Master.designer.cs
+++ TaxGenieOnline/Default.Master.designer.cs
@@ -58,6 +58,15 @@ namespace TaxGenieOnline {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cph_login;
         
         /// <summary>
+        /// imgADSrc control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgADSrc;
+        
+        /// <summary>
         /// txtSearch control.
         /// </summary>
         /// <remarks>
